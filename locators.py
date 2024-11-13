200740036f2acfcef88c8a076abd3d8aefab4857
Bransen Daniels <bransen.daniels@gmail.com>
2024-11-12 20:31:43 -0500

    Test Git Connection

--- locators.py
+++ locators.py
@@ -4,20 +4,36 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
+
 driver.get("https://demoqa.com/automation-practice-form")
 driver.maximize_window()
 #ID, Xpath, CSSSelectory, Classname, name, linkText
 
-driver.find_element(By.ID,"useremail").send_keys("hello@gmail.com")
-driver.find_element(By.ID).send_keys("hello@gmail.com")
+driver.find_element(By.ID,"firstName").send_keys("Bransen")
+driver.find_element(By.ID,"lastName").send_keys("Daniels")
+driver.find_element(By.ID,"userEmail").send_keys("hello@gmail.com")
+driver.find_element(By.ID,"userNumber").send_keys("4125555343")
+#driver.find_element(By.ID,"subjectsContainer").send_keys("My subjects")
+driver.find_element(By.ID,"currentAddress").send_keys("2720 College Park Rd")
 
+#driver.find_elemet(By.)
+
+driver.find_element(By.XPATH, "//label[@for ='gender-radio-1']").click()
+driver.find_element(By.XPATH, "//label[@for ='hobbies-checkbox-1']").click()
+driver.find_element(By.XPATH, "//button[@type= 'submit']").click()
+message = driver.find_element(By.ID, "example-modal-sizes-title-lg").text
+print(message)
 #test
 
+#Xpath example
+# //tagname[@attribute='value'] -->  //input[@type= 'submit']
+
+
+
+#//label[@for='gender-radio-1' and text()='Male']
+#//label[@for='gender-radio-1']
 
 
 
 
-
-
-
-time.sleep(10)
+time.sleep(20)
